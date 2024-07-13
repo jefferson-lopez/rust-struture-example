fbf9a8ff5d0eafc14e0e3bda30a115e8d954d1a9
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-13 15:13:22 -0600

    impls

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,39 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+    fn is_width(&self) -> bool {
+        return self.width > 0;
+    }
+    fn is_height(&self) -> bool {
+        return self.height > 0;
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
     dbg!(&rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("is Heigth {}", rect1.is_height());
+    println!("is Width {}", rect1.is_width());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
